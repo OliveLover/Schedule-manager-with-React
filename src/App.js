a2a6a5e47d7e56ea100e918e61b13966621a8265
tiktokyi <tiktokyi@gmail.com>
2024-06-12 22:20:39 +0900

    Create : TodoItem 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
+import TodoItem from "./component/TodoItem";
 import TodoList from "./component/TodoList";
 
 function App() {
