51ca0fc79a159fb7f3d46756432001204790f36c
tiktokyi <tiktokyi@gmail.com>
2024-06-13 20:09:31 +0900

    Feat : 빈입력 방지, 아이템 추가 후 입력 폼 초기화 하기

--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./TodoEditor.css";
 
 const TodoEditor = ({ onCreate }) => {
   const [content, setContent] = useState("");
+  const inputRef = useRef("");
+
   const onChangeContent = (e) => {
     setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!content) {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
   };
 
   return (
@@ -12,11 +23,12 @@ const TodoEditor = ({ onCreate }) => {
       <h4>새로운 Todo 작성하기 ✏️</h4>
       <div className="editor_wrapper">
         <input
+          ref={inputRef}
           value={content}
           onChange={onChangeContent}
           placeholder="새로운 Todo..."
         />
-        <button>추가</button>
+        <button onClick={onSubmit}>추가</button>
       </div>
     </div>
   );
