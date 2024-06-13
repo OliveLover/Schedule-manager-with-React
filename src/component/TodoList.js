619ba42f56779067998b6a7ce4e9a76b4eb75e7d
tiktokyi <tiktokyi@gmail.com>
2024-06-13 23:06:43 +0900

    Refactor : useMemo로 불필요한 함수 호출 제거

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
@@ -17,7 +17,8 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
         );
   };
 
-  const analyzeTodo = () => {
+  const analyzeTodo = useMemo(() => {
+    console.log("analyzeTodo 함수 호출");
     const totalCount = todo.length;
     const doneCount = todo.filter((it) => it.isDone).length;
     const notDoneCount = totalCount - doneCount;
@@ -27,9 +28,9 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
       doneCount,
       notDoneCount,
     };
-  };
+  }, [todo]);
 
-  const { totalCount, doneCount, notDoneCount } = analyzeTodo();
+  const { totalCount, doneCount, notDoneCount } = analyzeTodo;
 
   return (
     <div className="TodoList">
